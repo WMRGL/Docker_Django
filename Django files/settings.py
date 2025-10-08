7e9aef392912aeabaaf9a21d12f0051ecf92c049
ReubenMonaghan <reuben.monaghan@nhs.net>
2025-10-08 11:28:06 +0100

    environ added to settings.py

--- Django files/settings.py
+++ Django files/settings.py
@@ -1,2 +1,20 @@
+import os
+from pathlib import Path
+import environ as environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env('SECRET_KEY')
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
+
+DATABASES = {
+    'default': env.db('DEFAULT_URL'),
+    'Shire_Data': env.db('SHIRE_URL'),
+}
+
 STATIC_URL = 'static/'
 STATIC_ROOT = '/app/static/'
